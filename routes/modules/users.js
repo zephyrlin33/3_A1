112d3cd05cb7608858be53f4dc449fc7f1a8b14e
zephyr Lin <zephyrlin33@gmail.com>
2022-11-06 22:16:56 +0800

    revise registration method

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -41,43 +41,48 @@ router.post('/register', (req, res) => {
     if (password !== confirmPassword) {
         errors.push({ message: '密碼與確認密碼不相符！' })
     }
-
+	if (errors.length) {
+			return res.render('register', {
+				errors,
+				name,
+				email,
+				password,
+				confirmPassword
+			})
+		}
     // 檢查使用者是否已經註冊
     User.findOne({ email }).then(user => {
         // 如果已經註冊：退回原本畫面
         if (user) {
             errors.push({ message: '這個 Email 已經註冊過了。' })
-            console.log('這個 Email 已經註冊過了。')
-            res.render('register', {
+            //console.log('這個 Email 已經註冊過了。')
+
+            return res.render('register', {
+                errors,
                 name,
                 email,
                 password,
                 confirmPassword
             })
+            
+
         }
-
-        return bcrypt
-            .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
-            .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
-            .then(hash => User.create({
-                name,
-                email,
-                password: hash // 用雜湊值取代原本的使用者密碼
-            }))
-            .then(() => res.redirect('/'))
-            .catch(err => console.log(err))
+        else {
+            return bcrypt
+                .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+                .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+                .then(hash => User.create({
+                    name,
+                    email,
+                    password: hash // 用雜湊值取代原本的使用者密碼
+                }))
+                .then(() => res.redirect('/'))
+                .catch(err => console.log(err))
+        }
     })
-        .catch(err => console.log(err))
+        
 
-    if (errors.length) {
-        return res.render('register', {
-            errors,
-            name,
-            email,
-            password,
-            confirmPassword
-        })
-    }
+    
 
 })
 
