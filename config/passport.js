d500f8ca59efaeee4e9d45d9041bba7f7a2e867b
zephyr Lin <zephyrlin33@gmail.com>
2022-12-05 18:20:47 +0800

    modify

--- config/passport.js
+++ config/passport.js
@@ -10,22 +10,26 @@ module.exports = app => {
     app.use(passport.initialize())
     app.use(passport.session())
     // 設定本地登入策略
-    passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-        User.findOne({ email })
-            .then(user => {
-                if (!user) {
-                    return done(null, false, { message: 'That email is not registered!' })
-                }
-
-                return bcrypt.compare(password, user.password).then(isMatch => {
-                    if (!isMatch) {
-                        return done(null, false, { message: 'Email or Password incorrect!' })
+    passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passReqToCallback: true
+    },
+        (req, email, password, done) => {
+            User.findOne({ email })
+                .then(user => {
+                    if (!user) {
+                        return done(null, false, { message: 'That email is not registered!' })
                     }
-                    return done(null, user)
+
+                    return bcrypt.compare(password, user.password).then(isMatch => {
+                        if (!isMatch) {
+                            return done(null, false, req.flash('warning_msg', 'Incorrect email or password!'))
+                        }
+                        return done(null, user)
+                    })
                 })
-            })
-            .catch(err => done(err, false))
-    }))
+                .catch(err => done(err, false))
+        }))
 
     //Facebook登入策略
     passport.use(new FacebookStrategy({
